fb455aa4bc4c93a465ffde6c509a1b7a2da6e5a5
raulsan74 <123422220+raulsan74@users.noreply.github.com>
2023-03-13 22:27:43 +0100

    Update Driver.java
    Añadir documentación de los métodos (Raul)

--- arquetipoPatines/src/main/java/arquetipoPatines/Driver.java
+++ arquetipoPatines/src/main/java/arquetipoPatines/Driver.java
@@ -1,5 +1,11 @@
 package arquetipoPatines;
 
+
+/**
+ * Esta clase Driver define principalmente el comportamiento de pantalla para manejar 
+ * la aplicación Tienda de patinetes.
+ *
+ */
 public class Driver {
 	
 	static Tienda miTienda = new Tienda();
@@ -7,6 +13,11 @@ public class Driver {
 	 public Driver() {
 	    }
 
+	 /**
+     	* The main function of the program.
+     	*     	
+     	* @param args Pass arguments to the program when it is executed     	   	
+     	*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -46,6 +57,11 @@ public class Driver {
 
 	    }
 
+	/**
+     	* The Comprar function allows the user to buy a certain number of patinets from a given category.
+     	* The function first asks for the category, then it asks for how many patinets are wanted and finally it calls
+     	* the Comprar method in Tienda.java with those parameters. It also prints out how many were actually bought (which may be less than what was asked).
+     	*/
 	    static void Comprar() {
 	        int opcion = 0;
 
@@ -64,6 +80,11 @@ public class Driver {
 
 	    }
 
+	/**
+     	* The Alquilar function allows the user to rent a bike from the store.
+     	* The user can choose between renting a bike of any category, or one of three specific categories: Basic, Standard and Premium.
+     	* If there are no bikes available for rental in the chosen category (or if there are no bikes available at all), an error message is displayed.
+     	*/
 	    static void Alquilar() {
 	        int opcion = 0;
 
@@ -95,6 +116,15 @@ public class Driver {
 
 	    }
 
+	/**
+     	* The Devolver function allows the user to return a rented scooter.
+     	* The function asks for the reference number of the scooter and how many kilometers it has traveled.
+     	* If no kilometers have been traveled, then it cancels out of this function and returns to main menu.
+     	* Otherwise, if there are more than 0 km's traveled, then it will call on Devolver from Tienda class
+     	* which will check if that reference number exists in our array list or not. If so, then we update its status
+     	* as well as add those km's to its total mileage counter (kmTotal). 
+     	*
+     	*/
 	    static void Devolver() {
 	        String respuesta = "Se ha anulado la devolución";
 	        String codigo = ES.leeCadena("Introduzca el Número de Referencia del patinete a devolver: ");
@@ -113,6 +143,10 @@ public class Driver {
 	        MostrarEstadoTienda();
 	    }
 
+	/**
+     	* The Reparar function allows the user to repair a scooter by entering its reference number and the cost of repairing it.
+     	* If the cost is 0, then no repairs are made. Otherwise, if it's greater than 0, then repairs are made and a message is displayed on screen.
+     	*/
 	    static void Reparar() {
 	        String respuesta = "Se ha anulado la reparación";
 	        String codigo = ES.leeCadena("Introduzca el Número de Referencia del patinete a reparar: ");
@@ -131,6 +165,11 @@ public class Driver {
 	        MostrarEstadoTienda();
 	    }
 
+	 /**
+     	* The Vender function allows the user to sell a scooter.
+     	* The function asks for the reference number of the scooter, and then checks if it is in stock.
+     	* If it is, then it asks for confirmation from the user before selling it at its current price.
+     	*/
 	    static void Vender() {
 	        String respuesta = "Se ha anulado la venta";
 	        String codigo = ES.leeCadena("Introduzca el Número de Referencia del patinete a vender: ");
@@ -150,12 +189,24 @@ public class Driver {
 	        MostrarEstadoTienda();
 	    }
 
+	
+	/**
+    	* The BalanceGeneral function prints the balance general of the store.
+     	*/
 	    static void BalanceGeneral() {
 	        String respuesta = miTienda.balanceGeneral();
 	        MostrarMensajeConAlmohadillas(respuesta);
 	        MostrarEstadoTienda();
 	    }
 
+	
+	/**
+     	* The BalancePatinete function allows the user to balance a Patinete.
+     	* The function asks for the reference number of the Patinete, and then calls
+     	* miTienda.balancePatinete(codigo) to perform this action. If codigo is 0,
+     	* it cancels instead of balancing a Patinete. It then displays an appropriate message
+     	* using MostrarMensajeConAlmohadillas() and refreshes the screen with MostrarEstadoTienda().
+     	*/
 	    static void BalancePatinete() {
 	        String respuesta = "Se ha cancelado";
 	        String codigo = ES.leeCadena("Introduzca el Número de Referencia del patinete (0 para cancelar): ");
@@ -166,7 +217,13 @@ public class Driver {
 	        MostrarMensajeConAlmohadillas(respuesta);
 	        MostrarEstadoTienda();
 	    }
+	
 
+	 /**
+     	* The HistorialPatinete function allows the user to view the history of a given scooter.
+     	* The function asks for a reference number, and if it is not 0, then it will display
+     	* all of the information about that scooter's history. If 0 is entered, then nothing happens.
+     	*/	
 	    static void HistorialPatinete() {
 	        String respuesta = "Se ha cancelado";
 	        String codigo = ES.leeCadena("Introduzca el Número de Referencia del patinete (0 para cancelar): ");
@@ -177,13 +234,25 @@ public class Driver {
 	        MostrarMensajeConAlmohadillas(respuesta);
 	        MostrarEstadoTienda();
 	    }
+	
 
+	 /**
+     	* The MostrarEstadoTienda function displays the current state of the store.
+     	* It prints out a header, then calls toString() on miTienda and prints that out,
+     	* then it prints another footer.
+     	*/
 	    private static void MostrarEstadoTienda() {
 	        ES.msgln("######     Estado de la tienda      ######");
 	        ES.msgln(miTienda.toString());
 	        ES.msgln("######     Fin estado de la tienda  ######");
 	    }
 
+	/**
+     	* The MostrarMensajeConAlmohadillas function prints a message surrounded by #'s.
+     	*
+     	* @param mensaje Display a message to the user
+     	*
+     	*/
 	    private static void MostrarMensajeConAlmohadillas(String mensaje) {
 	        ES.msgln("#####################################################");
 	        ES.msgln(mensaje);
